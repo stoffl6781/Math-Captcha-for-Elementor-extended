153ce84b58fd4c14fd0ff105be73f24ad9acdf8f
Christoph Purin <christoph@purin.at>
2022-08-23 14:49:49 +0200

    added version

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@
 jQuery(document).ready(function($) {
 	//INIT FUNCTION
 		$(function(){
-	        $('.elementor-form').ebcaptcha();
-	    });
-		$(document).arrive(".elementor-form", function() {
-	        $('.elementor-form').ebcaptcha();
-	});
+			//$('my_options.class .elementor-form').ebcaptcha();
+			$(my_options.class + ' .elementor-form').ebcaptcha();
+		});
+		$(document).arrive(".elementor-popup-modal .elementor-form", function() {
+			$('.elementor-popup-modal .bs-math-captcha').remove();
+			$('.elementor-popup-modal .elementor-form').ebcaptcha();
+		});
 	//CAPTCHA FUNCTIONS
 	(function($){
 		jQuery.fn.ebcaptcha = function(options){
@@ -14,25 +16,38 @@ jQuery(document).ready(function($) {
 		var element = this; 
 		var submitcontainer = $(this).find('.elementor-field-type-submit')
 		var submit = $(this).find('button[type=submit]');
+		//GHOST CONTAINER FOR TRIGGERING CLICK EVENT
+		$('<div class="bs-submit-button-event"></div>').insertBefore(submit);
+			 $(".bs-submit-button-event").css({'width': (submit.outerWidth() + 'px')});
+			$(".bs-submit-button-event").css({'height': (submit.outerHeight() + 'px')});
 		$('<div class="bs-math-captcha elementor-field-type-text elementor-field-group elementor-column elementor-field-group-name elementor-col-100"><label id="bs_ebcaptchatext" class="elementor-field-label"></label><input type="text" class="elementor-field elementor-size-sm  elementor-field-textual" id="bs_ebcaptchainput" required data-error="Please solve the math problem."/></div>').insertBefore(submitcontainer);
 		var input = this.find('#bs_ebcaptchainput'); 
 		var label = this.find('#bs_ebcaptchatext'); 
 		
 		$(element).find('button[type=submit]').attr('disabled','disabled'); 
-
-		var operators = [{
-	        sign: "+",
-	        method: function(a,b){ return a + b; }
-	    	},{
+		var operators = [];
+		if(bs_math_captcha_plus_sign) {
+			operators.push(
+			{
+			sign: "+",
+	        method: function(a,b){ return a + b; }	
+			});
+		}
+		if(bs_math_captcha_minus_sign) {
+			operators.push(
+			{
 	        sign: "-",
 	        method: function(a,b){ return a - b; }
-   		},{
-	        sign: "*",
-	        method: function(a,b){ return a * b; }
-   		}];
+   			});
+		}
+		if(bs_math_captcha_multiply_sign) {
+			operators.push(
+			{
+			sign: "*",
+			method: function(a,b){ return a * b; }
+			});
+		}
    		var selectedOperator = Math.floor(Math.random()*operators.length);
-
-
 		var randomNr1 = 0; 
 		var randomNr2 = 0;
 		var totalNr = 0;
@@ -51,11 +66,15 @@ jQuery(document).ready(function($) {
 				$(element).find('button[type=submit]').removeAttr('disabled');
 				$(input).addClass('correct');
 				$(input).removeClass('error');
+				$('.bs-submit-button-event').css('z-index', '-1');
+				$('#errorcaptcha').hide();
+				$('#bs_ebcaptchainput').css('border-color', '');
 			}
 			else{
 				$(element).find('button[type=submit]').attr('disabled','disabled');
 				$(input).addClass('error');
 				$(input).removeClass('correct');
+				$('.bs-submit-button-event').css('z-index', '9');
 			}
 			
 		});
@@ -76,17 +95,4 @@ jQuery(document).ready(function($) {
 	};
 })
  (jQuery);
-	$('.elementor-field-type-submit').click(function(event){
-		    if ($(".elementor-field-type-submit .elementor-button").is(":disabled"))
-		    {
-		    	if ($("#errorcaptcha").length == 0) {
-		    		$('<p id="errorcaptcha">Math captcha Error</p>').insertBefore(this);
-		    		$('#bs_ebcaptchainput').css('border-color', 'red');
-		    	}
-		    }
-				else {
-					$('#errorcaptcha').hide();
-					$('#bs_ebcaptchainput').css('border-color', '');
-				}
-			});
 });
--- bs_math_captcha_for_elementor.php
+++ bs_math_captcha_for_elementor.php
@@ -1,24 +1,155 @@
 <?php
+
 /**
- * Plugin Name: Math Captcha for Elementor Forms
+ * Plugin Name: Math Captcha for Elementor Forms extended
  * Description: Simple match captcha for Elementor Page Builder Forms
  * Plugin URI: https://albanotoska.com/bsbanners/bs-math-captcha-for-elementor-forms/
- * Version: 0.3
- * Author: Albano Toska
+ * Version: 1.2.0
+ * Author: Albano Toska, Christoph Purin
  * Author URI: https://albanotoska.com/
  * License:     GPL2
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
  */
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 /* ENQUEUE NEEDED STYLES AND JS */
-function bs_match_captcha_for_elementor_forms_scripts_style() {
-			wp_register_script( 'mainjs', plugin_dir_url( __FILE__ ) . '/assets/js/main.js', array('jquery'), false, true );
-            wp_enqueue_script( 'mainjs' );
-            wp_register_script( 'ebrecaptcha', plugin_dir_url( __FILE__ ) . '/assets/js/jquery.ebcaptcha.js', array('jquery'), false, true );
-            wp_enqueue_script( 'ebrecaptcha' );
-            wp_register_script( 'arrivejs', plugin_dir_url( __FILE__ ) . '/assets/js/arrive.min.js', array('jquery'), false, true );
-            wp_enqueue_script( 'arrivejs' );
-	wp_register_style( 'maincss', plugin_dir_url( __FILE__ ) . '/assets/css/main.css');
-            wp_enqueue_style( 'maincss' );
+function bs_match_captcha_for_elementor_forms_scripts_style()
+{
+    wp_register_script('mainjs', plugin_dir_url(__FILE__) . '/assets/js/main.js', array('jquery'), false, true);
+    wp_enqueue_script('mainjs');
+    wp_register_script('ebrecaptcha', plugin_dir_url(__FILE__) . '/assets/js/jquery.ebcaptcha.js', array('jquery'), false, true);
+    wp_enqueue_script('ebrecaptcha');
+    wp_register_script('arrivejs', plugin_dir_url(__FILE__) . '/assets/js/arrive.min.js', array('jquery'), false, true);
+    wp_enqueue_script('arrivejs');
+    wp_register_style('maincss', plugin_dir_url(__FILE__) . '/assets/css/main.css');
+    wp_enqueue_style('maincss');
+
+    $options = get_option( 'bs_math_captcha_for_elementor_class' );
+
+    $scriptData = array(
+        'class' => $options,
+    );
+
+    wp_localize_script('mainjs', 'my_options', $scriptData);
+
 }
-add_action( 'wp_enqueue_scripts', 'bs_match_captcha_for_elementor_forms_scripts_style' );
+add_action('wp_enqueue_scripts', 'bs_match_captcha_for_elementor_forms_scripts_style');
+
+/* REGISTER SETTINGS */
+function bs_math_captcha_for_elementor_settings_register()
+{
+    add_option('bs_math_captcha_for_elementor_error', 'Error Label');
+    register_setting('bs_math_captcha_option_value', 'bs_math_captcha_for_elementor_error', 'myplugin_callback');
+    add_option('bs_math_captcha_signs_plus', 'Plus Checkbox');
+    register_setting('bs_math_captcha_option_value', 'bs_math_captcha_signs_plus', 'myplugin_callback');
+    add_option('bs_math_captcha_signs_minus', 'Minus Checkbox');
+    register_setting('bs_math_captcha_option_value', 'bs_math_captcha_signs_minus', 'myplugin_callback');
+    add_option('bs_math_captcha_signs_multiply', 'Multiply Checkbox');
+    register_setting('bs_math_captcha_option_value', 'bs_math_captcha_signs_multiply', 'myplugin_callback');
+
+    // changed
+    add_option('bs_math_captcha_for_elementor_class', 'Class names');
+    register_setting('bs_math_captcha_option_value', 'bs_math_captcha_for_elementor_class', 'myplugin_callback');
+}
+add_action('admin_init', 'bs_math_captcha_for_elementor_settings_register');
+
+/* OPTION PAGE */
+function bs_math_captcha_for_elementor_option_page()
+{
+    add_options_page('BS Math Captcha for Elementor', 'BS Math Captcha for Elementor', 'manage_options', 'bs_math_captcha_for_elementor_option', 'bs_math_captcha_for_elementor_option_page_content');
+}
+add_action('admin_menu', 'bs_math_captcha_for_elementor_option_page');
+
+/* CONTENT FOR OPTION PAGE */
+function bs_math_captcha_for_elementor_option_page_content()
+{
+?>
+    <div>
+        <?php screen_icon(); ?>
+        <h2>BS Math Captcha for Elementor</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('bs_math_captcha_option_value'); ?>
+            <h4>Please enter custom error message you want to show on invalid captcha</h4>
+            <table>
+                <tr valign="top">
+                    <td><input type="text" id="bs_math_captcha_for_elementor_error" name="bs_math_captcha_for_elementor_error" value="<?php echo get_option('bs_math_captcha_for_elementor_error'); ?>" /></td>
+                </tr>
+            </table>
+            <h4>Please enter the Form Class where you wish to active this Plugin.</h4>
+            <table>
+                <tr valign="top">
+                    <td><input type="text" id="bs_math_captcha_for_elementor_class" name="bs_math_captcha_for_elementor_class" value="<?php echo get_option('bs_math_captcha_for_elementor_class'); ?>" /></td>
+                </tr>
+            </table>
+
+            <h4> Please check which math logics to use</h4>
+            <table>
+                <tr valign="top">
+                    <td>
+                        <input type="checkbox" <?php if (get_option('bs_math_captcha_signs_plus')) echo 'checked' ?> id="bs_math_captcha_signs_plus" name="bs_math_captcha_signs_plus" value="+">
+                        <label for="bs_math_captcha_signs_plus">Plus(+)</label>&ensp;
+                        <input type="checkbox" <?php if (get_option('bs_math_captcha_signs_minus')) echo 'checked' ?> id="bs_math_captcha_signs_minus" name="bs_math_captcha_signs_minus" value="-">
+                        <label for="bs_math_captcha_signs_minus">Minus(-)</label>&ensp;
+                        <input type="checkbox" <?php if (get_option('bs_math_captcha_signs_multiply')) echo 'checked' ?> id="bs_math_captcha_signs_multiply" name="bs_math_captcha_signs_multiply" value="*">
+                        <label for="bs_math_captcha_signs_multiply">Multiply(*)</label>&ensp;
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+<?php
+}
+
+/* PLUGIN SETTINGS FROM PLUGIN LIST */
+function bs_math_captcha_for_elementor_settings_plugins($links)
+{
+    $settings_link = '<a href="options-general.php?page=bs_math_captcha_for_elementor_option">Settings</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+}
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", 'bs_math_captcha_for_elementor_settings_plugins');
+
+/* CHECK MATH CAPTCHA FUNCTION */
+function bs_math_captcha_for_elementor_error_messages()
+{
+    $custom_error_message = get_option('bs_math_captcha_for_elementor_error');
+    $custom_exlusion_class = get_option('bs_math_captcha_for_elementor_class');
+?>
+    <script>
+        var bs_math_captcha_plus_sign = false;
+        var bs_math_captcha_minus_sign = false;
+        var bs_math_captcha_multiply_sign = false;
+        <?php if (get_option('bs_math_captcha_signs_plus')) { ?>
+            bs_math_captcha_plus_sign = true;
+        <?php }
+        if (get_option('bs_math_captcha_signs_minus')) { ?>
+            bs_math_captcha_minus_sign = true;
+        <?php }
+        if (get_option('bs_math_captcha_signs_multiply')) { ?>
+            bs_math_captcha_multiply_sign = true;
+        <?php } ?>
+        jQuery(document).ready(function($) {
+            $(document).on('click', '.bs-submit-button-event', function(e) {
+                e.stopPropagation();
+                if ($(".elementor-field-type-submit .elementor-button").is(":disabled")) {
+                    $('#errorcaptcha').show();
+                    $('#bs_ebcaptchainput').css('border-color', 'red');
+                    if ($("#errorcaptcha").length == 0) {
+                        $('<p id="errorcaptcha"><?php if ($custom_error_message) {
+                                                    echo $custom_error_message;
+                                                } else {
+                                                    echo __('Math Captcha Error', 'math-captcha-for-elementor-forms');
+                                                } ?></p>').insertBefore('.elementor-field-type-submit');
+                        $('#bs_ebcaptchainput').css('border-color', 'red');
+                    }
+                } else {
+                    $('#errorcaptcha').hide();
+                    $('#bs_ebcaptchainput').css('border-color', '');
+                }
+            });
+        });
+    </script>
+<?php
+}
+add_action('wp_head', 'bs_math_captcha_for_elementor_error_messages');
